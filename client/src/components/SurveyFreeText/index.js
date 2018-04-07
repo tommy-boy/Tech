a0b541ba72b467b734e84390973142b56cf449fb
tommy-boy <digitalmedia.tr@gmail.com>
2018-04-07 06:07:57 -0700

    Update index.js

--- client/src/components/SurveyFreeText/index.js
+++ client/src/components/SurveyFreeText/index.js
@@ -1,36 +1,15 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { Field } from 'redux-form'
-import styled, {ThemeProvider} from 'styled-components'
-import { TextField } from 'material-ui';
+import { TextField } from 'material-ui'
 
 const styles = {        
-    textField: {
-      width: '80%',
-      paddingTop: 10,
-      display: 'block',
-      textAlign: 'left',    
-      'label + &': {
-        marginTop: 12,
-      },    
-    },
-    textFieldInput: {    
-      borderRadius: 2,
-      border: '1px solid #9B9B9B',
-      fontSize: '1.0em',
-      padding: '0px 12px',
-      width: 'calc(100% - 24px)',
-      transition: 'width 2s, height 2s, background-color 2s, transform 2s',
-      '&:focus': {
-        borderColor: '#80bdff',
-        boxShadow: '0 0 0 0.2rem rgba(0,123,255,.25)',
-      },
+  textField: {
+    textAlign: 'left',    
+    'label + &': {
+      marginTop: 12,
+      marginLeft: 12
     },    
-    hintTextStyle: {
-      top: 10,
-      left: 12,
-      fontSize: '0.85em',
-      color: 'rgba(0,0,0,0.38)'
-    }
+  },    
 };
 
 const renderFreeText = ({
@@ -39,24 +18,17 @@ const renderFreeText = ({
 }) => (
   <TextField                        
     style={styles.textField} 
-    underlineShow={false}
     placeholder="Write your comments here..."
-    hintStyle={styles.hintTextStyle}
-    multiLine={true}
+    multiline
     rows={4}
     rowsMax={8}
     fullWidth={true}
-    inputStyle={styles.textFieldInput}
     {...input}
   />
 )
 
 class FreeText extends React.Component {
   
-    constructor(props) {
-      super(props);
-    }
-
     render() {
       return (  
         <Field name={`postiveFeedback_${this.props.id}`} component={renderFreeText} type="text" />
