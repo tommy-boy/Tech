f4603dc91890421f03dfbc86f06860fc72d060e0
tommy-boy <digitalmedia.tr@gmail.com>
2018-04-07 09:35:50 -0700

    Update index.js

--- client/src/components/App/index.js
+++ client/src/components/App/index.js
@@ -1,11 +1,12 @@
 import React from 'react'
-import SurveyTemplate from '../SurveyTemplate'
+import { Route } from 'react-router-dom'
 import { MuiThemeProvider } from 'material-ui/styles';
 import { survey } from '../../themes'
+import SurveyTemplate from '../SurveyTemplate'
 
 const App = () => {    
     return (
-        <MuiThemeProvider muiTheme={survey}>
+        <MuiThemeProvider theme={survey}>
             <SurveyTemplate />
         </MuiThemeProvider>
     )
