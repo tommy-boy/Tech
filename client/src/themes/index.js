e7d4ec0d006b662e9552f85c57728306aecf287d
tommy-boy <digitalmedia.tr@gmail.com>
2018-04-07 05:15:56 -0700

    Update index.js

--- client/src/themes/index.js
+++ client/src/themes/index.js
@@ -1,5 +1,40 @@
-import { createMuiTheme } from 'material-ui/styles'
+import { createTheme } from '../utils/theme'
 
-export const createTheme = (theme) => {
-    return createMuiTheme(theme)
-}
+export const survey = createTheme({
+  palette: {
+    primary: {
+        light: '#ff903b',
+        main: '#094AA8',
+        dark: '#a43100',
+        contrastText: '#000000',
+    },
+    secondary: {
+        light: '#797979',
+        main: '#4d4d4d',
+        dark: '#252525',
+        contrastText: '#ffffff'
+    },
+    error: {
+      light: '#E57373',
+      main: '#F44336',
+      dark: '#D32F2F',
+      contrastText: '#F44336',
+    },
+  },
+  overrides: {
+    MuiInput: {
+      underline: {
+        '&:before': {
+          backgroundcolor: '#094AA8'
+        },
+        '&:hover:not($disabled):before': { 
+          backgroundColor: '#0d6cf2',
+        },
+      }
+    }
+  },
+  status: {
+    danger: 'orange',
+  },
+});
+
