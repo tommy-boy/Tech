10ca67c8865eb8ff0602ce82f7489afd7e51b363
tommy-boy <digitalmedia.tr@gmail.com>
2018-04-08 16:07:09 -0700

    Update index.js

--- client/src/components/InstagramIcon/index.js
+++ client/src/components/InstagramIcon/index.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import styled from 'styled-components'
+import instagramIcon from '../Shared/icons/Instagram.png'
 
 const Icon = styled.img`
+    margin-right: 8px;
     padding-left: 8px;
     padding-right: 8px;
     border-right: 1px solid #ffffff;
     vertical-align: middle;
 `;
 
-export default () => <Icon src="../../static/icons/Instagram.png" />
+export default () => <Icon src={instagramIcon} />
