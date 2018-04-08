26d87ea5bd842005e0af3a01ffbf5cb079fa4a66
tommy-boy <digitalmedia.tr@gmail.com>
2018-04-08 16:05:09 -0700

    Update index.js

--- client/src/components/EdmundsIcon/index.js
+++ client/src/components/EdmundsIcon/index.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import styled from 'styled-components'
+import edmundsIcon from '../Shared/icons/Edmunds.png'
 
 const Icon = styled.img`
+    margin-right: 8px;
     padding-left: 8px;
     padding-right: 8px;
     border-right: 1px solid #ffffff;
     vertical-align: middle;
 `;
 
-export default () => <Icon src="../../static/icons/Edmunds.png" />
+export default () => <Icon src={edmundsIcon} />
