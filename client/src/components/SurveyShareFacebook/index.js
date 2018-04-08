cc5e4bddd5f9d1719e78f139008e657237d3d3f1
tommy-boy <digitalmedia.tr@gmail.com>
2018-04-08 16:10:39 -0700

    Update index.js

--- client/src/components/SurveyShareFacebook/index.js
+++ client/src/components/SurveyShareFacebook/index.js
@@ -6,13 +6,15 @@ import ActionFacebook from '../FacebookIcon';
 const styles = { 
   flatbutton: {
     marginBottom: 12,
+    paddingLeft: 0,
     width: '100%',
     color: '#FFFFFF',  
     backgroundColor: '#3B5998',            
     hoverColor: 'transparent',
     verticalAlign: 'middle',
     textTransform: "capitalize",
-    textAlign: 'left',   
+    textAlign: 'left',
+    justifyContent: 'initial'   
   },
   title: {  
     color: '#FFFFFF',      
