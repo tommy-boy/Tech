ee9b76d4fb31ba02009f7ac84a6ca7283ac4c646
tommy-boy <digitalmedia.tr@gmail.com>
2018-04-08 16:07:50 -0700

    Update index.js

--- client/src/components/TwitterIcon/index.js
+++ client/src/components/TwitterIcon/index.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import styled from 'styled-components'
+import twitterIcon from '../Shared/icons/Twitter.png'
 
 const Icon = styled.img`
+    margin-right: 8px;
     padding-left: 8px;    
     padding-right: 8px;
     border-right: 1px solid #ffffff;
     vertical-align: middle;
 `;
 
-export default () => <Icon src="../Shared/icons/Twitter.png" />
+export default () => <Icon src={twitterIcon} />
