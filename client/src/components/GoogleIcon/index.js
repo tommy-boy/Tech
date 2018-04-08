cdeaa793c76a820091dc7994f4db43acdcead0bb
tommy-boy <digitalmedia.tr@gmail.com>
2018-04-08 16:06:24 -0700

    Update index.js

--- client/src/components/GoogleIcon/index.js
+++ client/src/components/GoogleIcon/index.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import styled from 'styled-components'
+import googleIcon from '../Shared/icons/Google.png'
 
 const Icon = styled.img`
+    margin-right: 8px;
+    padding-left: 8px;
     padding-right: 8px;
     border-right: 1px solid #ffffff;
     vertical-align: middle;
 `;
 
-export default () => <Icon src="../../static/icons/Google.png" />
+export default () => <Icon src={googleIcon} />
