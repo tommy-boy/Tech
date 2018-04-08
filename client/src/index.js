6742ae9a2f00a057234586f210f3e150fcc8e652
tommy-boy <digitalmedia.tr@gmail.com>
2018-04-08 04:44:18 -0700

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import {BrowserRouter} from 'react-router-dom';
 import registerServiceWorker from './registerServiceWorker'
 import store from './store'
 import './index.css';
 import Root  from './components/Root'
 
-ReactDOM.render(<Root store={store} />, document.getElementById('root'));
+ReactDOM.render((
+    <BrowserRouter>
+        <Root store={store} />
+    </BrowserRouter>
+    ), document.getElementById('root'));
 registerServiceWorker();
