805829fda293b66033f548fccf34eff7771014a9
tommy-boy <digitalmedia.tr@gmail.com>
2018-04-08 11:09:03 -0700

    Update index.js

--- client/src/components/SurveyShareButtons/index.js
+++ client/src/components/SurveyShareButtons/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import ShareEdmundsButton from '~/SurveyShareEdmunds'
-import ShareFacebookButton from '~/SurveyShareFacebook'
-import ShareGoogleButton from '~/SurveyShareGoogle'
-import ShareCarsButton from '~/SurveyShareCars'
+import ShareEdmundsButton from '../SurveyShareEdmunds'
+import ShareFacebookButton from '../SurveyShareFacebook'
+import ShareGoogleButton from '../SurveyShareGoogle'
+import ShareCarsButton from '../SurveyShareCars'
 
 class ShareButtons extends React.Component {
   
