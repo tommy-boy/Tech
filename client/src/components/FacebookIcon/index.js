b131d9770dc484b7dcdf253d2754ae76d0cd4b3e
tommy-boy <digitalmedia.tr@gmail.com>
2018-04-08 16:05:50 -0700

    Update index.js

--- client/src/components/FacebookIcon/index.js
+++ client/src/components/FacebookIcon/index.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import styled from 'styled-components'
+import facebookIcon from '../Shared/icons/Facebook.png'
 
 const Icon = styled.img`
+    margin-right: 8px;
     padding-left: 8px;    
     padding-right: 8px;
     border-right: 1px solid #ffffff;
     vertical-align: middle;
 `;
 
-export default () => <Icon src="../../static/icons/Facebook.png" />
+export default () => <Icon src={facebookIcon} />
