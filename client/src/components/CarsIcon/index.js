3d24566e154afe083d92378cac9abd749cb93a84
tommy-boy <digitalmedia.tr@gmail.com>
2018-04-08 16:04:27 -0700

    Update index.js

--- client/src/components/CarsIcon/index.js
+++ client/src/components/CarsIcon/index.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import styled from 'styled-components'
+import carsIcon from '../Shared/icons/Cars.com.png'
 
 const Icon = styled.img`
+    margin-right: 8px;
     padding-left: 8px;
     padding-right: 8px;
     border-right: 1px solid #ffffff;
     vertical-align: middle;
 `;
 
-export default () => <Icon src="../Shared/icons/Cars.com.png"  />
+export default () => <Icon src={carsIcon}  />
