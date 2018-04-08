bb9c041719ea2ab102ca2f344f759cc727cf6ee7
tommy-boy <digitalmedia.tr@gmail.com>
2018-04-08 11:11:10 -0700

    Update index.js

--- client/src/components/SurveyShareGoogle/index.js
+++ client/src/components/SurveyShareGoogle/index.js
@@ -1,49 +1,33 @@
 import React from 'react';
-import Link from 'next/link'
-import FlatButton from 'material-ui/FlatButton';
-import ActionGoogle from '~/GoogleIcon';
-import styled from 'styled-components';
+import { Link } from 'react-router-dom'
+import Button from 'material-ui/Button';
+import ActionGoogle from '../GoogleIcon';
 
 const styles = { 
   flatbutton: {
     marginBottom: 12,
-    paddingLeft: 8,
-    width: '100%',  
-    backgroundColor: '#DB3236',            
+    width: '100%',
+    color: '#FFFFFF',  
+    backgroundColor: '#3B5998',            
     hoverColor: 'transparent',
     verticalAlign: 'middle',
-    textAlign: 'left',   
+    textTransform: "capitalize",
+    textAlign: 'left',     
   },
   title: {  
     color: '#FFFFFF',      
-  },
-  label: {    
-    fontSize: '0.9em',    
-    color: '#FFFFFF',
-    textTransform: 'normal',
-  },
+  }  
 };
 
 class ShareGoogle extends React.Component {
 
-  constructor(props) {
-    super(props);
-   
-  }
-
   render() {
     return (      
       <div>       
-        <Link href='https://plus.google.com'>
-          <a target="_blank">
-            <FlatButton 
-              style={styles.flatbutton} 
-              labelStyle={styles.label} 
-              label="Share your review on Google"
-              labelPosition="after"
-              icon={<ActionGoogle />}
-            />
-          </a>
+        <Link to='https://plus.google.com' target="_blank" style={{ textDecoration: 'none' }} > 
+            <Button 
+              style={styles.flatbutton}               
+            ><ActionGoogle />Share your review on Google</Button>
         </Link>         
       </div>
     );
