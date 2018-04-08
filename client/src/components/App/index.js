f1ca6a3728ffbb5f092ba9bb4a40fb1298f0be7d
tommy-boy <digitalmedia.tr@gmail.com>
2018-04-08 04:45:22 -0700

    Update index.js

--- client/src/components/App/index.js
+++ client/src/components/App/index.js
@@ -1,13 +1,20 @@
 import React from 'react'
-import { Route } from 'react-router-dom'
 import { MuiThemeProvider } from 'material-ui/styles';
 import { survey } from '../../themes'
+import { Route, Switch } from 'react-router-dom'
 import SurveyTemplate from '../SurveyTemplate'
+import SurveyPositiveFeedback from '../SurveyPositiveFeedback'
+import SurveyNegativeFeedback from '../SurveyNegativeFeedback'
 
 const App = () => {    
     return (
         <MuiThemeProvider theme={survey}>
-            <SurveyTemplate />
+            <Switch>
+                <Route path="/" component={SurveyTemplate} />
+                <Route path="/plus/" component={SurveyPositiveFeedback} />
+                <Route path="/minus/" component={SurveyNegativeFeedback} />
+                <Route render={() => <h1>Page not found</h1>} />
+            </Switch>
         </MuiThemeProvider>
     )
 }
